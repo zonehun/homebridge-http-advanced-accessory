a70acb3a6d983332e2a88eaabb4071d35c4e21c9
CT Tsai <cttsai@qnap.com>
2017-06-12 16:13:54 +0800

    correct code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var Service, Characteristic;
 var request = require("request");
 var pollingtoevent = require('polling-to-event');
-var homekitExtensions = require('./homekit-extensions.js'),
+var homekitExtensions = require('./homekit-extensions.js');
 
 module.exports = function (homebridge) {
 	Service = homebridge.hap.Service;
